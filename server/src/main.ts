9ccc6d65f0f2a1b2879c2afcee31349ea92136d5
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-03-29 16:28:20 -0700

    remove options from nest factory

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,14 +5,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:8080',
-        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-      ],
-    },
-  });
+  const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const port = parseInt(configService.get('PORT'));
